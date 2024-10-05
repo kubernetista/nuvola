49f02fc5388f01eafb1f4173dfa8fbfd38b87c48
Alfredo Colangelo <6275539+acola@users.noreply.github.com>
2024-10-05 17:03:53 +0200

    update gitea runners

--- scripts/gitea-runner.sh
+++ scripts/gitea-runner.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
 # export RUNNER_TOKEN="<token>"
-echo ${RUNNER_TOKEN}
-# export RUNNER_TOKEN_B64=$(echo ${RUNNER_TOKEN} | base64)
-# echo ${RUNNER_TOKEN_B64}
+echo "Gitea Runner Token: ${RUNNER_TOKEN}"
+export RUNNER_TOKEN_B64=$(echo ${RUNNER_TOKEN} | base64)
+echo -e "Gitea Runner Token base64: ${RUNNER_TOKEN_B64}\n"
+
+RUNNER_DIR="gitea-runner"
+RUNNER_DEPOLYMENT="deploy-rootless.yaml"
+RUNNER_DEPOLYMENT_DIND="deploy-dind.yaml"
+
 
 # Update 📋  the token in the Runner deployment
-envsubst < ./gitea-runner/rootless.yaml.template > ./gitea-runner/rootless.yaml
+
+if [ -f "./${RUNNER_DIR}/${RUNNER_DEPOLYMENT}.template" ]; then
+    envsubst < ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT}.template     > ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT}
+else
+  echo "File ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT}.template does not exist."
+fi
+
+if [ -f "./${RUNNER_DIR}/${RUNNER_DEPOLYMENT_DIND}.template" ]; then
+    envsubst < ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT_DIND}.template     > ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT_DIND}
+else
+  echo "File ./${RUNNER_DIR}/${RUNNER_DEPOLYMENT_DIND}.template does not exist."
+fi
+
+envsubst < ./${RUNNER_DIR}/secret-runner-token.yaml.template > ./${RUNNER_DIR}/secret-runner-token.yaml
